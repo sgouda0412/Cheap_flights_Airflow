8676170aed09426162315075dee62f859f3dd4d8
sgouda0412 <sgouda0412@gmail.com>
2024-10-06 08:48:23 +0530

    commit it

--- dags/dag_flight_search.py
+++ dags/dag_flight_search.py
@@ -1,13 +1,23 @@
 try:
     # Import libraries
-    from flights_searcher import search_flights                         # Function for API request
+    from flights_searcher import search_flights  # Function for API request
     from datetime import timedelta, datetime
     from airflow import DAG
-    from airflow.operators.python_operator import PythonOperator        # Python operator to execute functions 
-    from airflow.operators.email_operator import EmailOperator          # Email operator to send email
-    from airflow.operators.python_operator import BranchPythonOperator  # Branch operator to check conditions
-    from airflow.operators.dummy_operator import DummyOperator          # Dummy operator to do nothing when condition fail
-    from airflow.models.variable import Variable                        # Function to get environmental variables from airflow
+    from airflow.operators.python_operator import (
+        PythonOperator,
+    )  # Python operator to execute functions
+    from airflow.operators.email_operator import (
+        EmailOperator,
+    )  # Email operator to send email
+    from airflow.operators.python_operator import (
+        BranchPythonOperator,
+    )  # Branch operator to check conditions
+    from airflow.operators.dummy_operator import (
+        DummyOperator,
+    )  # Dummy operator to do nothing when condition fail
+    from airflow.models.variable import (
+        Variable,
+    )  # Function to get environmental variables from airflow
 
     # Setting up Triggers
     from airflow.utils.trigger_rule import TriggerRule
@@ -16,69 +26,77 @@ try:
 
 except Exception as e:
     print("Error  {} ".format(e))
+    print("Everything is good....")
 
 
-
-#-------------------------------------------------------------------------
-THRESHOLD = 8000  # MXN threshold value to consider a round flight as cheap  
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+THRESHOLD = 8000  # MXN threshold value to consider a round flight as cheap
+# -------------------------------------------------------------------------
 
 
 # Function to evaluate flight prices
 def evaluate_flight_prices(**context):
-    ti = context['ti']
-    flights_data = ti.xcom_pull(task_ids='Search_flights')
+    ti = context["ti"]
+    flights_data = ti.xcom_pull(task_ids="Search_flights")
     for index, flight in flights_data.iterrows():
-        if flight['Price_mxn'] <= THRESHOLD:
-            return 'Send_email'
-    return 'Do_nothing'
+        if flight["Price_mxn"] <= THRESHOLD:
+            return "Send_email"
+    return "Do_nothing"
+
 
 # Function to call when DAG fails
 def on_failure_callback(context):
-    '''Function to execute in case there is an error during DAG running'''
+    """Function to execute in case there is an error during DAG running"""
 
     print("Fail works  !  ")
 
 
 # DAG definition
-with DAG(dag_id="Flight_search",
-         schedule_interval="@once",                         # How frequently the DAG is going to run
-         default_args={
-             "owner": "airflow",
-             "start_date": datetime(2024, 4, 10),           # First date of execution
-             "retries": 1,
-             "retry_delay": timedelta(minutes=1),           # How much time must pass to re-run after failure
-             'on_failure_callback': on_failure_callback,
-             'email': ['jc.barreras.maldonado@gmail.com'],  
-             'email_on_failure': False,
-             'email_on_retry': False,
-         },
-         catchup=False) as dag:
+with DAG(
+    dag_id="Flight_search",
+    schedule_interval="@once",  # How frequently the DAG is going to run
+    default_args={
+        "owner": "airflow",
+        "start_date": datetime(2024, 4, 10),  # First date of execution
+        "retries": 1,
+        "retry_delay": timedelta(
+            minutes=1
+        ),  # How much time must pass to re-run after failure
+        "on_failure_callback": on_failure_callback,
+        "email": ["jc.barreras.maldonado@gmail.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+    },
+    catchup=False,
+) as dag:
 
     # DAG to request API flights information
     search_flights_task = PythonOperator(
-        task_id='Search_flights',
+        task_id="Search_flights",
         python_callable=search_flights,
         provide_context=True,  # Needed if context is required for the function to work
-        op_kwargs={'origin': 'HMO', 'destination': 'MEX', 'api_token': Variable.get("API_FLIGHTS_SECRET_TOKEN")}  # Provide search_flights() arguments
+        op_kwargs={
+            "origin": "HMO",
+            "destination": "MEX",
+            "api_token": Variable.get("API_FLIGHTS_SECRET_TOKEN"),
+        },  # Provide search_flights() arguments
     )
 
     # DAG to evaluate flight prices and decide whether to send email or not
     evaluate_flight_prices_task = BranchPythonOperator(
-        task_id='Evaluate_flight_prices',
+        task_id="Evaluate_flight_prices",
         python_callable=evaluate_flight_prices,
         provide_context=True,
     )
 
-
     # DAG to send email if a cheap flight is found
     send_email_task = EmailOperator(
-        task_id='Send_email',
-        to='jc.barreras.maldonado@gmail.com',
-        subject='Airflow Alert: Vuelos Baratos Encontrados',
-        #provide_context=True,  # Indicar a Airflow que proporcione el contexto
-        html_content=""" 
-        <h3>Se han encontrado vuelos baratos!</h3> 
+        task_id="Send_email",
+        to="jc.barreras.maldonado@gmail.com",
+        subject="Airflow Alert: Vuelos Baratos Encontrados",
+        # provide_context=True,  # Indicar a Airflow que proporcione el contexto
+        html_content="""
+        <h3>Se han encontrado vuelos baratos!</h3>
         <p>A continuación se muestra la información de los vuelos:</p>
         <table border="1">
             <tr>
@@ -112,9 +130,7 @@ with DAG(dag_id="Flight_search",
     )
 
     # Define la tarea Do_nothing
-    do_nothing_task = DummyOperator(
-        task_id='Do_nothing'
-    )
+    do_nothing_task = DummyOperator(task_id="Do_nothing")
 
     # Define dependencias entre tareas
     search_flights_task >> evaluate_flight_prices_task
